093fe4d46f92cd6244536c998721617fc4fac102
Hermann <hermann.dzouavele-ikya@r3-group.fr>
2026-03-26 09:32:25 +0100

    Fix terraform formatting

--- terraform/security.tf
+++ terraform/security.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -20,12 +20,12 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
-  description = "HTTPS"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     description = "Grafana"
